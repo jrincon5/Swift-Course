f92feb5761f6a366ecbfb7465dcc28b5c77f3669
Julián Rincón Usma - Ceiba Software <julian.rincon@Soportes-Mac-mini-2.local>
2018-04-13 11:19:24 -0500

    Probando el scope y acceso de control

---
+++ AccesControl.playground/Contents.swift
@@ -0,0 +1,7 @@
+//Nivel de acceso Open: Cualquier puede tener acceso
+//Nivel de acceso Public: Tiene el mismo nivel de acceso que Open, pero la restricción que tiene es que solo puede hacerlo dentro de su módulo.
+//Nivel de acceso Internal: por ejemplo cuando creamos una función.
+//Nivel de acceso File-Private: acceso únicamente del archivo fuente donde fue declarado.
+//Nivel de acceso Private: acceso únicamente a la declaración y sus extensiones.
+
+var cursoSwift = Curso("Cursito", "/img/src/123.jpg");
